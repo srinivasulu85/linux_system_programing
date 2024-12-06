24c296b4d2b90c9ca9ff7968023c0580d3a6d7a4
srinivasulu <srinivasulud220@gmail.com>
2024-12-06 16:41:22 +0530

    Added fork example test code

--- process_management/fork.c
+++ process_management/fork.c
@@ -1,10 +1,26 @@
 #include<stdio.h>
 
+#define CHILD 0
+
 int main()
 {
-	int pid = fork();
+      	int status;
 
 	printf("%d\n",getpid());
 
+	int pid = fork();
+	
+        if(pid == CHILD)
+	{
+	  printf("This is child pid: %d \n",getpid());
+	}
+	else
+	{
+
+           wait(&status);
+	
+           printf("This is parent Pid: %d \n",getpid());
+	}
+           
 	//  while(1);
 }
